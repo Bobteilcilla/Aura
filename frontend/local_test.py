0d151970e34bb21c612fa268a87040f4aecebb62
riccardo pellizzon <r.p.archetype@gmail.com>
2025-11-20 10:26:26 +0000

    local test file

--- frontend/local_test.py
+++ frontend/local_test.py
@@ -1,9 +1,9 @@
 from package_aura.linreg_model import linreg_model_predict
 
 # Example test values
-noise = 10.1
-light = 20.2
-crowd = 10.1
+noise_db = 10.1
+light_lux = 20.2
+crowd_count = 10.1
 
-result = linreg_model_predict(noise, light, crowd)
+result = linreg_model_predict(noise_db, light_lux, crowd_count)
 print("Prediction result:", result)
