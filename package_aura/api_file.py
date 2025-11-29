3ea8117179c9fdbb6bc6a95826ec4c17093b4d10
Steven Adler <steven_adler@gmx.de>
2025-11-29 16:23:40 +0100

    Added model_prefix as argument to predict endpoint, default = gb_models

--- package_aura/api_file.py
+++ package_aura/api_file.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
 import pickle
 
-from package_aura.hello_aura import hello_aura
-from package_aura.linreg_model import linreg_model_predict
-from package_aura.gradient_boosting import gradient_boosting_predict
+from package_aura.model_functions import model_predict
 
 # FastAPI instance
 app = FastAPI()
@@ -13,21 +11,11 @@ app = FastAPI()
 def root():
     return {'greeting':"AURA is up and running!"}
 
-# Test endpoint
-@app.get("/hello")
-def hello():
-
-    # Return greeting from hello_aura function
-    return {"greeting": hello_aura()}
-
 # Prediction endpoint
 @app.get("/predict")
-def predict(noise_db, light_lux, crowd_count, model_type: str = "linreg"):
-    if model_type == "linreg":
-        print("Received model_type:", model_type)
-        return linreg_model_predict(noise_db, light_lux, crowd_count)
-    elif model_type == "gb":
-        print("Received model_type:", model_type)
-        return gradient_boosting_predict(noise_db, light_lux, crowd_count)
-    else:
-        return {"error": "Invalid model_type. Choose 'linreg' or 'gb'."}
+def predict(noise_db, light_lux, crowd_count, model_prefix: str = "gb_models"):
+    if model_prefix not in ("linreg_models", "gb_models"):
+        print("Model not found:", model_prefix)
+        raise ValueError("Model not found:", model_prefix)
+        return None
+    return model_predict(noise_db, light_lux, crowd_count, model_prefix)
