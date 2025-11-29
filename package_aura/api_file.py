ffc20945768c76e0d34d086e47bffc8a56035c69
Riccardo Pellizzon <@users.noreply.github.com>
2025-11-29 13:03:34 +0000

    predict edited

--- package_aura/api_file.py
+++ package_aura/api_file.py
@@ -50,15 +50,6 @@ def hello():
 # -------------------------- #
 
 @app.get("/predict")
-def predict(noise_db, light_lux, crowd_count, model_type: str = "linreg"):
-    if model_type == "linreg":
-        print("Received model_type:", model_type)
-        return linreg_model_predict(noise_db, light_lux, crowd_count)
-    elif model_type == "gb":
-        print("Received model_type:", model_type)
-        return gradient_boosting_predict(noise_db, light_lux, crowd_count)
-    else:
-        return {"error": "Invalid model_type. Choose 'linreg' or 'gb'."}
 def predict(noise_db: float, light_lux: float, crowd_count: float):
     """
     Main AURA prediction endpoint.
